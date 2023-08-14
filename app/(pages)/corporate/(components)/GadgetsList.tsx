67b537b3ced3f4dfdb1a12dae394bdfa9ef9d13b
Dmytro Komlyk <dima.komlyk@gmail.com>
2023-08-14 08:54:52 +0300

    refactor: delete array numbers, fix font-size

--- app/(pages)/corporate/(components)/GadgetsList.tsx
+++ app/(pages)/corporate/(components)/GadgetsList.tsx
@@ -20,18 +20,20 @@ interface IGadgetsListProps {
   items: IGadgetItem[]
 }
 
-const gadgetsViewLaptopOrder = [1, 3, 5, 2, 4, 6]
-
 export const GadgetsList: React.FC<IGadgetsListProps> = ({ items }) => {
   const windowSize = useWindowSize()
 
   const sortedItems =
     windowSize.width > 768
-      ? items.sort(
-          (a, b) =>
-            gadgetsViewLaptopOrder.indexOf(a.id) -
-            gadgetsViewLaptopOrder.indexOf(b.id),
-        )
+      ? items.sort((a, b) => {
+          if (a.id % 2 === 0 && b.id % 2 !== 0) {
+            return 1
+          }
+          if (a.id % 2 !== 0 && b.id % 2 === 0) {
+            return -1
+          }
+          return a.id - b.id
+        })
       : items
   return (
     <div className='flex flex-wrap gap-6'>
@@ -42,7 +44,7 @@ export const GadgetsList: React.FC<IGadgetsListProps> = ({ items }) => {
             className='flex min-w-[206px] max-w-[270px] shrink grow items-center gap-4'
           >
             <Image src={item.icon.src} width={40} height={40} alt={item.alt} />
-            <p className='hyphens-auto text-center text-xl text-[#F8FCFF]'>
+            <p className='hyphens-auto text-center font-exo_2 text-xl text-[#F8FCFF]'>
               {item.title}
             </p>
           </div>
