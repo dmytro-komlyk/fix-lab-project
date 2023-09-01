c21e35d46e37b834b87c6b003e7129c58adda910
Kozlov Evgeniy <dp200890kev@gmail.com>
2023-09-01 14:02:33 +0300

    Changed styles component

--- app/(pages)/repair/(components)/CategoriesSection.tsx
+++ app/(pages)/repair/(components)/CategoriesSection.tsx
@@ -41,41 +41,42 @@ const CategoriesSection: React.FC<CategoriesSectionProps> = ({
     setShowInstantAdviceModal(prev => !prev)
   }, [setShowInstantAdviceModal])
   return (
-    <section className='section pt-[163px]'>
-      <div className='container'>
+    <section className='section py-[163px]'>
+      <div className='container flex flex-col gap-8 md:px-0'>
         <div className='flex items-center gap-1'>
           <Link
             className='flex items-center gap-1 text-base font-[400] text-[#3EB9F0]'
             href='/'
           >
-            <p> Головна</p> <MdKeyboardArrowRight size={30} />
+            <p> Головна</p>
+            <MdKeyboardArrowRight size={30} />
           </Link>
 
           <p className='text-base font-[400] text-[#3EB9F0] opacity-70'>
             Ремонт
           </p>
         </div>
-        <div className='justify-between gap-8 md:flex'>
-          <div className='mb-8 max-w-[300px] md:mb-0'>
-            <h3 className='mb-8 font-exo_2 text-xl font-bold leading-normal text-light-blue md:text-2xl'>
+        <div className='flex justify-between gap-8 max-md:flex-col'>
+          <div className='flex w-[300px] flex-col gap-8 md:mb-0'>
+            <h3 className='font-exo_2 text-xl font-bold leading-normal text-light-blue md:text-2xl'>
               Що зламалося?
             </h3>
-            <p className='mb-4'>
+            <p className='text-base font-normal'>
               У нас є багато варіантів, як подарувати друге життя вашому
               гаджету.
             </p>
-            <p className='mb-4'>
-              У нас є багато варіантів, як подарувати друге життя вашому
-              гаджету.
+            <p className='text-base font-normal'>
+              Обирайте потрібний пристрій, що зламався, та дізнавайтесь ціни на
+              ремонт.
             </p>
-            <p className='mb-8'>
+            <p className='text-base font-normal'>
               Або ж, економте час, залишайте заявку на консультацію.
             </p>
             <Button
               text='Миттєва консультація'
               toggleModal={toggleInstantAdviceModal}
-              styles='group relative flex min-w-[256px] min-h-[56px] items-center justify-center rounded-2xl bg-mid-green transition-colors  hover:bg-mid-blue focus:bg-mid-blue  max-md:w-full'
-              textHoverAnimation='py-5 text-base font-semibold tracking-wide text-dark-blue group-hover:animate-hoverBtnOut animate-hoverBtnIn'
+              styles='group relative flex w-[256px] py-4 items-center justify-center rounded-2xl bg-mid-green transition-colors  hover:bg-mid-blue focus:bg-mid-blue  max-md:w-full'
+              textHoverAnimation='text-base font-semibold tracking-wide text-dark-blue group-hover:animate-hoverBtnOut animate-hoverBtnIn'
             />
           </div>
 
