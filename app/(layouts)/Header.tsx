427f98ec838d0d0fe06178497d9e916d0e46b754
Kozlov Evgeniy <dp200890kev@gmail.com>
2023-09-01 13:28:55 +0300

    Changed styles in Button call courier

--- app/(layouts)/Header.tsx
+++ app/(layouts)/Header.tsx
@@ -268,8 +268,8 @@ export const Header: React.FC = () => {
           <Button
             text='Викликати курʼєра'
             toggleModal={toggleCourierModal}
-            styles='group relative flex min-w-[256px] min-h-[56px] items-center justify-center rounded-2xl bg-mid-green transition-colors  hover:bg-mid-blue focus:bg-mid-blue  max-md:w-full'
-            textHoverAnimation='py-5 text-base font-semibold tracking-wide text-dark-blue group-hover:animate-hoverBtnOut animate-hoverBtnIn'
+            styles='group relative flex min-w-[256px] py-4 items-center justify-center rounded-2xl bg-mid-green transition-colors  hover:bg-mid-blue focus:bg-mid-blue  max-md:w-full'
+            textHoverAnimation='text-base font-semibold tracking-wide text-dark-blue group-hover:animate-hoverBtnOut animate-hoverBtnIn'
           />
         </div>
 
