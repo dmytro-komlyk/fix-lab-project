bee0b326a424a1fcc01c19a0d474709b5139fb92
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2023-09-07 12:07:49 +0200

    merged with dev and fixed font of the button

--- app/(components)/CommonButton/CommonButton.tsx
+++ app/(components)/CommonButton/CommonButton.tsx
@@ -14,7 +14,7 @@ const CommonButton: React.FC<ICommonButtonProps> = ({
     onClick={onClick}
     className='group relative z-[1] flex w-full items-center justify-center rounded-[12px] bg-mid-green pb-[21px] pt-[22px] transition-colors hover:bg-mid-blue focus:bg-mid-blue'
   >
-    <span className='animate-slide-bottom text-base font-semibold leading-none tracking-wide text-[#04268b] group-hover:animate-slide-top'>
+    <span className='animate-slide-bottom font-inter text-base font-semibold leading-none tracking-wide text-[#04268b] group-hover:animate-slide-top'>
       {children}
     </span>
   </button>
