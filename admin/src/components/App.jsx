1e799b446b3ef070feaa82b10c98dd17f94c57f0
Dmitriy <dima.komlyk@gmail.com>
2023-09-29 13:45:39 +0300

    feat: added brand components logic

--- admin/src/components/App.jsx
+++ admin/src/components/App.jsx
@@ -1,6 +1,7 @@
 "use client"; // only needed if you choose App Router
 import { Admin, Resource } from "react-admin";
 import dataProvider from "../dataProvider";
+import { CreateBrand, EditBrand, ListBrands } from "./Brand";
 import { CreateContact, EditContact, ListContacts } from "./Contact";
 import { CreateGadget, EditGadget, ListGadgets } from "./Gadget";
 import { CreateIssue, EditIssue, ListIssues } from "./Issue";
@@ -19,7 +20,12 @@ const AdminApp = () => (
       edit={EditIssue}
       create={CreateIssue}
     />
-    {/* <Resource name="brands" list={ListBrands} edit={EditBrand} /> */}
+    <Resource
+      name="brands"
+      list={ListBrands}
+      edit={EditBrand}
+      create={CreateBrand}
+    />
     <Resource
       name="contacts"
       list={ListContacts}
--- admin/src/components/Brand.jsx
+++ admin/src/components/Brand.jsx
@@ -1,36 +1,104 @@
-import * as React from "react";
+import { RichTextInput } from "ra-input-rich-text";
 import {
+  BooleanField,
+  BooleanInput,
   Create,
   Datagrid,
   Edit,
+  ImageField,
+  ImageInput,
   List,
   TabbedForm,
+  TextField,
+  TextInput,
   useRecordContext,
 } from "react-admin";
 
-const TitleBrand = () => {
+const TitleBrand = ({ text }) => {
   const record = useRecordContext();
-  return <span>Бренд {record ? `"${record.title}"` : ""}</span>;
+  return (
+    <span>
+      {text} {record ? `"${record.title}"` : ""}
+    </span>
+  );
 };
 
 const ListBrands = () => {
   return (
-    <List title="Brands">
-      <Datagrid rowClick="edit"></Datagrid>
+    <List title="Бренди">
+      <Datagrid rowClick="edit">
+        <ImageField source="icon.src" title="Зображення" />
+        <TextField source="slug" label="Slug" />
+        <TextField source="title" label="Назва" />
+        <BooleanField label="isActive" source="Активний" />
+      </Datagrid>
     </List>
   );
 };
 
-const EditBrand = () => (
-  <Edit title={<TitleBrand />}>
-    <TabbedForm></TabbedForm>
-  </Edit>
-);
+const EditBrand = () => {
+  return (
+    <Edit title={<TitleBrand text="Редагування бренду" />}>
+      <TabbedForm>
+        <TabbedForm.Tab label="Контент">
+          <TextInput disabled source="id" />
+          <TextInput source="slug" label="Slug" />
+          <TextInput source="title" label="Заголовок" />
+          <ImageInput source="icon" label="Зображення">
+            <ImageField source="src" />
+          </ImageInput>
+          <RichTextInput source="article" label="Cтаття" />
+          <BooleanInput source="isActive" label="Активний" />
+        </TabbedForm.Tab>
+        <TabbedForm.Tab label="SEO">
+          <TextField source="title" label="Назва" />
+          <TextInput source="metadata.title" label="SEO title" fullWidth />
+          <TextInput
+            source="metadata.description"
+            label="SEO description"
+            fullWidth
+          />
+          <TextInput
+            source="metadata.keywords"
+            label="SEO keywords"
+            fullWidth
+          />
+        </TabbedForm.Tab>
+      </TabbedForm>
+    </Edit>
+  );
+};
 
-const CreateBrand = () => (
-  <Create>
-    <TabbedForm></TabbedForm>
-  </Create>
-);
+const CreateBrand = () => {
+  return (
+    <Create title={<TitleBrand text="Новый бренд" />}>
+      <TabbedForm>
+        <TabbedForm.Tab label="Контент">
+          <TextInput source="slug" label="Slug" />
+          <TextInput source="title" label="Заголовок" />
+          <ImageInput source="icon" label="Зображення">
+            <ImageField source="src" />
+          </ImageInput>
+          <RichTextInput source="article" label="Cтаття" />
+          <BooleanInput source="isActive" label="Активний" />
+        </TabbedForm.Tab>
+        <TabbedForm.Tab label="SEO">
+          <TextField source="title" label="Назва" />
+          <TextInput source="metadata.title" label="SEO title" fullWidth />
+          <TextInput
+            source="metadata.description"
+            label="SEO description"
+            fullWidth
+          />
+          <TextInput
+            source="metadata.keywords"
+            label="SEO keywords"
+            fullWidth
+          />
+        </TabbedForm.Tab>
+      </TabbedForm>
+    </Create>
+  );
+};
 
 export { CreateBrand, EditBrand, ListBrands };
