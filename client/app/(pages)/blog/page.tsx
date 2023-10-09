da54f6445b62a10345d4af4a3edba734a2094fce
DimaHavr <dimaroztoky@gmail.com>
2023-10-10 01:54:46 +0300

    update

--- client/app/(pages)/blog/page.tsx
+++ client/app/(pages)/blog/page.tsx
@@ -1,4 +1,4 @@
-import getData from '@/app/(server)/api/service/helpers/fetchDataFromServer'
+import getData from '@/app/(server)/api/service/getData'
 
 import BlogsSection from './(components)/BlogsSection'
 
