59a7f8418ce319b4874fd39fcff03289be86480c
DimaHavr <dimaroztoky@gmail.com>
2023-10-11 18:23:20 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   images: {
-    domains: ['res.cloudinary.com', '95.217.34.212'],
+    domains: ['res.cloudinary.com', '95.217.34.212:30000'],
   },
 }
 
