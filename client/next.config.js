f516fe07056277fad3fc051cf6ad3fb0cd0589d0
DimaHavr <dimaroztoky@gmail.com>
2023-10-17 00:49:29 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -5,9 +5,14 @@ const nextConfig = {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   images: {
-    domains: ['95.217.34.212'],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '95.217.34.212',
+        port: '30000',
+        pathname: '/public/**',
+      },
+    ],
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 }
