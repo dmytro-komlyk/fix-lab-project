82a9858d040d961d510eae0404fa93871b44283a
Dmytro Komlyk <dima.komlyk@gmail.com>
2023-10-16 14:27:34 +0300

    Update contacts.controller.ts

--- server/src/domain/contacts/contacts.controller.ts
+++ server/src/domain/contacts/contacts.controller.ts
@@ -62,7 +62,7 @@ export class ContactsController {
   @ApiOperation({ summary: 'update existing Contact by ID' })
   @ApiResponse({ status: 200, type: Contact })
   @ApiResponse({ status: 404, description: 'Contact was not found' })
-  @Patch('/:id')
+  @Put('/:id')
   public async update(
     @Param('id') id: string,
     @Body() dto: UpdateContactDto
