f6d962364b4d5c142cce3541cf25a939eaa83dfd
DimaHavr <dimaroztoky@gmail.com>
2023-10-18 02:08:18 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -5,11 +5,8 @@ const nextConfig = {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   images: {
-    images: {
-      domains: ['95.217.34.212', '95.217.34.212:30000'],
-      loader: 'default',
-      path: 'http://95.217.34.212:30000/public/',
-    },
+    domains: ['95.217.34.212'],
+    unoptimized: true,
   },
 }
 
