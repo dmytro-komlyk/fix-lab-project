5cb67ee7e27194c06acc0d9f4216183c4f72158f
DimaHavr <dimaroztoky@gmail.com>
2023-10-21 22:06:35 +0300

    Update route.ts

--- client/app/(server)/api/revalidate/route.ts
+++ client/app/(server)/api/revalidate/route.ts
@@ -1,16 +1,21 @@
-import { revalidatePath } from 'next/cache'
+import { revalidateTag } from 'next/cache'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest) {
-  if (request.nextUrl.searchParams.get('secret') !== 'fixlab') {
-    return new Response(`Invalid credentials`, {
-      status: 500,
-    })
+// e.g a webhook to `your-website.com/api/revalidate?tag=collection&secret=<token>`
+export async function POST(request: NextRequest) {
+  const secret = request.nextUrl.searchParams.get('secret')
+  const tag = request.nextUrl.searchParams.get('telefon')
+
+  if (secret !== 'fixlab') {
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
   }
-  const path = request.nextUrl.searchParams.get('path') as string
-  revalidatePath(path)
-  return NextResponse.json({
-    revalidate: true,
-  })
+
+  if (!tag) {
+    return NextResponse.json({ message: 'Missing tag param' }, { status: 400 })
+  }
+
+  revalidateTag(tag)
+
+  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
