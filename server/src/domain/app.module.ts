da537f45c84bd5b9aa60341f0c4b983e85020a4f
Vitalii Zavhorodnii <49337282+vitalii-zavhorodnii@users.noreply.github.com>
2023-10-26 14:26:33 +0200

    feat: added serving static for frontend application

--- server/src/domain/app.module.ts
+++ server/src/domain/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
+import * as path from 'path';
+
+import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
 import { BenefitsModule } from './benefits/benefits.module';
 import { BrandsModule } from './brands/brands.module';
@@ -10,11 +14,11 @@ import { GadgetsModule } from './gadgets/gadgets.module';
 import { ImagesModule } from './images/images.module';
 import { IssuesModule } from './issues/issues.module';
 import { UsersModule } from './users/users.module';
-import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
