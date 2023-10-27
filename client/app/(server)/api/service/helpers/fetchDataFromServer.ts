e982a61a87ea45a7ed059a78f347d340caaf6ea7
Vitalii Zavhorodnii <49337282+vitalii-zavhorodnii@users.noreply.github.com>
2023-10-27 21:22:57 +0200

    fix: client changed fetch

--- client/app/(server)/api/service/helpers/fetchDataFromServer.ts
+++ client/app/(server)/api/service/helpers/fetchDataFromServer.ts
@@ -2,22 +2,14 @@
 // const apiUrl = process.env.NEXT_PUBLIC_SERVER_URL
 
 // SSG //
+import axios from 'axios'
+
+export const revalidate = 60
 
 export default async function fetchDataFromServer(url: string) {
-  try {
-    const res = await fetch(`http://95.217.34.212:30000/api${url}`, {
-      next: { revalidate: 60 },
-    })
+  const { data } = await axios.get(`http://95.217.34.212:30000/api${url}`)
 
-    if (!res.ok) {
-      throw new Error(res.status.toString() + res.statusText)
-    }
-
-    return await res.json()
-  } catch (error) {
-    console.error('Error fetching data:', error)
-    throw error
-  }
+  return data
 }
 
 // SSR //
--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-  },
-  images: {
-    domains: ['95.217.34.212'],
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
