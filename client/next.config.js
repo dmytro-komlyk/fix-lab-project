5820d5e119b7d43bec228d2ac1b874cdd1d0161b
Vitalii Zavhorodnii <49337282+vitalii-zavhorodnii@users.noreply.github.com>
2023-10-28 01:24:32 +0200

    fix: rolled back all changes

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
+  images: {
+    domains: ['95.217.34.212'],
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
