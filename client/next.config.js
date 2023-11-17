e3be62cb153027fdeb76fba27078b260e8b75634
DimaHavr <dimaroztoky@gmail.com>
2023-11-17 15:40:57 +0200

    feat: update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    incrementalCacheHandlerPath: require.resolve('./cache-handler.js'),
-    isrMemoryCacheSize: 0, // disable default in-memory caching
-  },
   env: {
     NEXT_PUBLIC_BASE_URL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     NEXT_PUBLIC_SERVER_URL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
