bf0fb57033f93ba3536c2d44aa92887da6fb5664
DimaHavr <dimaroztoky@gmail.com>
2023-11-17 04:35:23 +0200

    feat: update brands page, optimize hero section, add revalidate route and isr config,

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    incrementalCacheHandlerPath: require.resolve('./cache-handler.js'),
+    isrMemoryCacheSize: 0, // disable default in-memory caching
+  },
   env: {
     NEXT_PUBLIC_BASE_URL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     NEXT_PUBLIC_SERVER_URL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
@@ -9,6 +13,7 @@ const nextConfig = {
     domains: ['95.217.34.212'],
     unoptimized: true,
   },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
