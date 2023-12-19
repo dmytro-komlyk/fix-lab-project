3a405a760942fd372a210f278242f9bbcdbd2479
Dmitriy <dima.komlyk@gmail.com>
2023-12-19 23:41:07 +0200

    refactor: updated app module

--- server/src/domain/app.module.ts
+++ server/src/domain/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { NotificationsModule } from './notifications/notifications.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { TrpcModule } from './trpc/trpc.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TrpcModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TrpcModule,
+    PrismaModule,
+    UsersModule,
+    NotificationsModule
+  ],
   providers: [PrismaService]
 })
 export class AppModule {}
