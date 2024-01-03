cd8b001139e58c0642106d19b791c5c0cfbcb6f3
Dmitriy <dima.komlyk@gmail.com>
2024-01-04 00:17:05 +0200

    refactor: update admin trpc

--- admin/app/(utils)/trpc/client.ts
+++
@@ -1,4 +0,0 @@
-import { createTRPCReact } from '@trpc/react-query'
-import { AppRouter } from 'server/src/domain/trpc/trpc.router'
-
-export const trpc = createTRPCReact<AppRouter>({})
--- admin/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Home() {
-  return (
-    <main className=' flex h-full w-full flex-auto'>
-      <section className=' bg-footer-gradient-linear-blue flex w-full items-center justify-center  overflow-hidden  pb-[102px] pt-[163px] max-md:pb-14 max-md:pt-[120px]'>
-        <h1 className=' bold font-exo_2 text-white-dis text-4xl'>Hello</h1>
-      </section>
-    </main>
-  )
-}
