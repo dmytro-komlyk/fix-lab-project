4ca4c51a85690e0cea38672d48d20053c58911bf
Dmitriy <dima.komlyk@gmail.com>
2024-01-24 00:11:57 +0200

    refactor: updated contacts form

--- apps/admin/app/(pages)/contacts/page.tsx
+++ apps/admin/app/(pages)/contacts/page.tsx
@@ -1,17 +1,28 @@
+import { SERVER_URL } from '@admin/app/(lib)/constants'
+import { auth } from '@admin/app/(utils)/authOptions'
 import { serverClient } from '@admin/app/(utils)/trpc/serverClient'
-import type { outputContactSchema } from '@server/domain/contacts/schemas/contact.schema'
+import type { outputContactSchema as IContact } from '@server/domain/contacts/schemas/contact.schema'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export const dynamic = 'force-dynamic'
 
 const ContactsPage = async () => {
-  const contactsData =
-    (await serverClient.contacts.getAllContacts()) as outputContactSchema[]
+  const session = await auth()
+  const user = session?.user ? session.user : null
+  const contactsData = (await serverClient({
+    user: {
+      id: 'string',
+      email: 'string',
+      name: 'string',
+      accessToken: 'string',
+      accessTokenExpires: 0,
+    },
+  }).contacts.getAllContacts()) as IContact[]
 
   return (
     <main className='flex flex-auto'>
-      <section className='flex h-[100vh] w-full  bg-footer-gradient-linear-blue py-[60px]'>
+      <section className='flex h-[100vh] w-full bg-footer-gradient-linear-blue py-[60px]'>
         <div className='container relative flex flex-col items-center justify-center px-8'>
           <ul className='flex items-center justify-center gap-6 '>
             {contactsData.map(item => (
@@ -19,7 +30,7 @@ const ContactsPage = async () => {
                 <Link href={`/contacts/${item.id}`}>
                   <Image
                     className='h-[140px] w-[220px] object-contain  object-center'
-                    src={`${process.env.NEXT_PUBLIC_IMAGES_BASE_URL}/public/pictures/${item.image.file.filename}`}
+                    src={`${SERVER_URL}/${item.image.file.path}`}
                     width={300}
                     height={200}
                     alt={item.image.alt}
