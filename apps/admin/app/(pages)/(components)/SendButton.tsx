4cc720dff01a95495c3fee8fb4013370add0cb65
Dmitriy <dima.komlyk@gmail.com>
2024-01-25 11:10:04 +0200

    refactor: updated components

--- apps/admin/app/(pages)/(components)/SendButton.tsx
+++ apps/admin/app/(pages)/(components)/SendButton.tsx
@@ -30,7 +30,7 @@ const SendButton: React.FC<SendButtonProps> = ({
       type={type}
       isLoading={isLoading}
       disabled={disabled}
-      className='group flex w-[320px] justify-center rounded-2xl bg-mid-blue px-6 py-4 text-white-dis transition duration-300 hover:scale-[1.03] hover:bg-black-dis focus:scale-[1.03] focus:bg-black-dis'
+      className='flex w-[20em] h-[4em] justify-center rounded-2xl bg-mid-blue px-6 py-4 text-white-dis transition duration-300 hover:scale-[1.03] hover:bg-black-dis focus:scale-[1.03] focus:bg-black-dis'
     >
       <span className='text-center font-exo_2 text-xl font-bold'>Зберегти</span>
     </Button>
