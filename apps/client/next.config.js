a33b4ea4a6eca51e2ba4d0e918ab2c1042025f48
Dmitriy <dima.komlyk@gmail.com>
2024-01-30 20:46:16 +0200

    refactor: updated next image conf

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
     return config
   },
   images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '95.217.34.212',
+        port: '30000',
+        pathname: '/public/**',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
   },
   output: 'standalone',
