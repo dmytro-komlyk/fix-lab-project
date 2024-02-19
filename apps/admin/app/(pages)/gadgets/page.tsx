67713c00a763e0eb540bf0f5c512abffac53c311
Dmitriy <dima.komlyk@gmail.com>
2024-02-19 15:52:19 +0200

    refactor: added validation and updated styles

--- apps/admin/app/(pages)/gadgets/page.tsx
+++ apps/admin/app/(pages)/gadgets/page.tsx
@@ -16,7 +16,7 @@ const GadgetsPage = async () => {
 
   return (
     <main className='flex h-full flex-auto'>
-      <section className=' flex w-full justify-center bg-footer-gradient-linear-blue py-[60px]'>
+      <section className='flex w-full justify-center bg-footer-gradient-linear-blue py-[60px]'>
         <div className='relative flex flex-col items-center justify-center'>
           <GadgetsList gadgetsData={gadgetsData} />
         </div>
